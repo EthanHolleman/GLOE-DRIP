a6e9fd471eee327a2a7bb8fd7970e61d3867547e
Your Name <etholleman@ucdavis.edu>
2021-04-27 13:05:43 -0700

    rework metaplot script for better plots / more control and andd python script for creating design files

---
+++ scripts/design.py
@@ -0,0 +1,46 @@
+# Read an SRA table with sample information and create a "design"
+# table that can be used by the R metagene package for plotting
+# by experimental group. In the sra file the experimental group
+# should be under a column called "modification" and the sample
+# name should be under "Sample Name" (case sensitive).
+
+import pandas as pd 
+import argparse
+from pathlib import Path
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sra', help='Path to sra table to read.')
+    parser.add_argument('out', help='Path to output table.')
+    parser.add_argument('--delim', default=',', help='Delimiter of sra file.')
+
+    return parser.parse_args()
+
+
+def read_sra(filepath, delim=',', **kwargs):
+    return pd.read_csv(str(filepath), sep=delim, **kwargs)
+
+
+def design_frame(sra_frame):
+    groups = list(set(list(sra_frame['modification'])))
+    group_matrix = {}
+    for index, row in sra_frame.iterrows():
+        sample_row = {g: 0 for g in groups}
+        sample_row[str(row['modification'])] = 1
+        group_matrix[str(row['Sample Name'])] = sample_row
+    return pd.DataFrame(group_matrix).transpose()
+
+
+def main():
+    args = get_args()
+    sra_df = read_sra(args.sra)
+    design_df = design_frame(sra_df)
+    design_df.to_csv(args.out, sep='\t')
+
+if __name__ == '__main__':
+    main()
+    
+        
+
+
+    
--- scripts/metagene.R
+++ scripts/metagene.R
@@ -1,24 +1,70 @@
 library(metagene)
 data(promoters_hg19)
+library(ggplot2)
+library(ggpubr)
+library(RColorBrewer)
 
 
+
+read_design_table <- function(table.path, named_bam_files){
+    print('Reading table')
+    design.df <- as.data.frame(read.table(table.path))
+    samples <- rownames(design.df)
+    rownames(design.df) <- named_bam_files[samples]
+    design.df$Samples <- rownames(design.df)
+    design.df <- design.df[,order(ncol(design.df):1)]
+    rownames(design.df) <- c()
+    design.df
+
+}
+
+plot_metagene <- function(metagene.df){
+    print('Plotting')
+    metagene.df$design <- as.factor(metagene.df$design)
+
+    ggplot(metagene.df, aes(x=bin, y=value, ymin=qinf, ymax=qsup)) + 
+    geom_ribbon(aes(fill=design), alpha=0.3) +
+    geom_line(aes(color=design), size=1) + facet_wrap(~group) + 
+    scale_color_brewer(palette="Dark2") +
+    theme_pubr()
+           
+}
 
 # bam files should be specified with name in command line args
 main <- function(){
 
     args <- commandArgs(trailingOnly=TRUE)
     output_path <- args[1]
+    
     args.length <- length(args)  # should be even first half is bam files second is names 
-    args.no_out <- args[2:length(args)]
+    args.no_out <- args[3:length(args)]
     midpoint <- length(args.no_out) / 2
+
     bam_files <- args.no_out[1:midpoint]
     bam_names <- args.no_out[(midpoint+1):length(args.no_out)]
-    print(bam_names)
     names(bam_files) <- bam_names
+    
+
+    design_df <- read_design_table(args[2], bam_files)
+    print('adding design table')
+    design_df$Samples <- paste0(system.file("extdata", package="metagene"), "/",
+                        design_df$Samples)
+    print('added design')
+    print(design_df)
+    print(bam_files)
     mg <- metagene$new(regions=promoters_hg19, bam_files=bam_files)
-    plt <- mg$plot(title='2kb promotor aligned GLOE-seq reads')
-    ggsave(output_path, plt, dpi=500)
+    print('made metagene promotors')
+    mg.df <- mg$get_data_frame()
+    print('got df')
+    print(head(mg.df))
+    write.table(mg.df, paste(args.output_path, 'tsv', sep='.'))
+    plt <- plot_metagene(mg.df)
+    ggsave(output_path, plt, dpi=500, height=10, width=12)
 }
+
+
+
+
 
 if (! interactive()){
     main()
