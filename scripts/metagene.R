832552cc16135b647b67d131d9e6f0d64139e532
Your Name <etholleman@ucdavis.edu>
2021-04-26 21:39:04 -0700

    update arg parsing

--- scripts/metagene.R
+++ scripts/metagene.R
@@ -4,26 +4,24 @@ data(promoters_hg19)
 
 
 # bam files should be specified with name in command line args
-
-
-
 main <- function(){
 
     args <- commandArgs(trailingOnly=TRUE)
     output_path <- args[1]
-    args.length <- length(args)-1  # should be even first half is bam files second is names 
-    bam_files <- args[2:args.length/2]
-    bam_names <- args[args.length/2:args.length]
+    args.length <- length(args)  # should be even first half is bam files second is names 
+    args.no_out <- args[2:length(args)]
+    midpoint <- length(args.no_out) / 2
+    bam_files <- args.no_out[1:midpoint]
+    bam_names <- args.no_out[(midpoint+1):length(args.no_out)]
+    print(bam_names)
     names(bam_files) <- bam_names
-    mg <- metagene$new(regions=promoters_hg19, bam_files=bam_files, flip_regions=TRUE)
+    mg <- metagene$new(regions=promoters_hg19, bam_files=bam_files)
     plt <- mg$plot(title='2kb promotor aligned GLOE-seq reads')
     ggsave(output_path, plt, dpi=500)
 }
 
-metagene_plot <- function(bed)
+if (! interactive()){
+    main()
+}
 
-
-
-# get the bam files to plot
-# name the bam files based on command line inputs 
 
